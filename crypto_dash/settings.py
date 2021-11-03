c4f3555da540d26f9de3afd1f71053d334ac41aa
Pratik Goswami <prtkgoswami8@gmail.com>
2021-11-02 17:39:04 -0700

    Error Fixes

--- crypto_dash/settings.py
+++ crypto_dash/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import sys
 import django_heroku
-import rest_framework
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
